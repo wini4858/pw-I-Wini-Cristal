9570b585e88ef4eba37119f8ca2a933d9fc2393d
wini cristal <winicristalnina@gmail.com>
2024-03-28 20:40:41 -0300

    aula funtions

---
+++ aula04/ex1.php
@@ -0,0 +1,10 @@
+<?php
+
+     function soma($x, $y) {
+        $z = $x + $y;
+        return $z;
+      }
+
+      echo "22 + 7 = " . soma(22,7);
+      
+?>
---
+++ aula04/functions.php
@@ -0,0 +1,17 @@
+<?php
+    function Natsu() {
+  echo "<b> Conforme o sol negro afunda...<br> os raios de um amanhã claro <br> começam a brilhar<b><br> <br>"; 
+}
+
+Natsu();
+
+    function familyName($fname) {
+        echo "$fname Siles.<br>";
+    }
+
+    familyName("João");
+    familyName("Lais");
+    familyName("Bug");
+
+
+?>
